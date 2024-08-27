2cefd737c5812f28743fc91b72e2222b414e6e17
AnjaliBind12345 <anbind3724@gmail.com>
2024-08-27 11:39:15 +0530

    Updated endpoints

--- src/App.jsx
+++ src/App.jsx
@@ -36,3 +36,41 @@ const App = () => {
 
 export default App;
 
+// import React, { Suspense, lazy } from 'react';
+// import { Routes, Route } from 'react-router-dom';
+// import ProtectedRoute from './components/ProtectedRoute';
+// import './App.css';
+
+// // Lazy load the modules
+// const Login = lazy(() => import('./pages/Login/Login'));
+// const TeamMembers = lazy(() => import('./pages/TeamMembers/TeamMembers'));
+// const Training = lazy(() => import('./pages/Training/Training'));
+// const Dashboard = lazy(() => import('./pages/Dashboard/Dashboard'));
+// const WFO = lazy(() => import('./pages/WFO/WFO'));
+// const RootLayout = lazy(() => import('./pages/Root/Root'));
+
+// const App = () => {
+//   return (
+//     <Suspense fallback={<div>Loading...</div>}>
+//       <Routes>
+//         {/* Adjust the paths to match your live URL structure */}
+//         <Route path="/login" element={<Login />} />
+//         <Route
+//           path="/*"
+//           element={
+//             <ProtectedRoute>
+//               <RootLayout />
+//             </ProtectedRoute>
+//           }
+//         >
+//           <Route path="" element={<Dashboard />} />
+//           <Route path="teammembers" element={<TeamMembers />} />
+//           <Route path="wfo" element={<WFO />} />
+//           <Route path="training" element={<Training />} />
+//         </Route>
+//       </Routes>
+//     </Suspense>
+//   );
+// };
+
+// export default App;
